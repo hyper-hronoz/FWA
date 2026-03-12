4924fb7f8e11d2da04865383344a00ebf48c0734
Hronoz <vlad_moryakov_prog@mail.ru>
2026-03-12 22:35:46 +0300

    MERGED branches

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,4 +1,5 @@
-export const API_BASE_URL = 'http://192.168.0.105:3001'
+// export const API_BASE_URL = 'http://192.168.0.105:3001'
+export const API_BASE_URL = 'http://localhost:3001'
 
 export const ROUTES = {
   auth: {
